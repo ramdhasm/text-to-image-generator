0547022bee77f379accc01a1c00d568b5ac56ff7
mramdhas <147613958+mramdhas@users.noreply.github.com>
2023-11-06 13:44:26 +0100

    Initial Commit

---
+++ TextToImageGen/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TextToImageGen
+//
+//  Created by Ramdhas on 2023-11-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TextToImageGen/TextToImageGenApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextToImageGenApp.swift
+//  TextToImageGen
+//
+//  Created by Ramdhas on 2023-11-06.
+//
+
+import SwiftUI
+
+@main
+struct TextToImageGenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
